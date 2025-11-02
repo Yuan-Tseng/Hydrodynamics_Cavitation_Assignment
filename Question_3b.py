6e4a5c4ed22f810cb7ab2e9b17280ef8100d7579
Yuan <tsengy@ethz.ch>
2025-11-02 20:48:41 +0100

    Changed method calculating A

--- Question_3b.py
+++ Question_3b.py
@@ -21,14 +21,14 @@ def capillary_time():
     return np.sqrt(density * R0**3 / surface_tension)
 
 def dispersion_omega():
-    '''Dispersion relation ω(k)'''
+    '''Dispersion relation ω(k) (imaginary part only)'''
     omega_sq = (-1 / capillary_time()**2) * ((k*R0 * iv(1, k*R0)) / (iv(0, k*R0)) * (1 - (k*R0)**2))
     omega = np.lib.scimath.sqrt(omega_sq)
     return omega
 
 def radial_deformation():
     '''Radial deformation R = R0 + R' at reference positions x_ref and time t'''
-    R_prime = h * np.exp(1j * ((k - dispersion_omega()/jet_velocity)*x_ref - k*jet_velocity*t))
+    R_prime = np.real(h * np.exp(1j * ((k - dispersion_omega()/jet_velocity)*x_ref - k*jet_velocity*t)))
     return R0 + R_prime.real
 
 def pressure_variation():
@@ -45,10 +45,10 @@ def pressure_variation():
 
     # Boundary condition 
     pR = surface_tension * (k**2 * h - h / R0**2)
+    A = pR / iv(0, k * R0)
 
     for i, R_now in enumerate(R_profile):
         r_mask = r <= R_now  # mask for points inside the jet at this x_ref
-        A = pR / iv(0, k * R_now)
         p_hat_r = A * iv(0, k * r[r_mask])
         p_prime_grid[i, r_mask] = np.real(p_hat_r * np.exp(1j * phi[i]))
 
@@ -65,10 +65,11 @@ if __name__ == "__main__":
     contour = plt.contourf(x_ref*100, r*1000, p_prime.T, 100, cmap='coolwarm')  # transpose for (r, x)
     plt.colorbar(contour, label='Pressure [Pa]')
     plt.xlabel(r'$x_{ref}$ [cm]', fontsize=13)
-    plt.ylabel(r'[mm]', fontsize=13)
+    plt.ylabel(r'$radius$ [mm]', fontsize=13)
     # plt.title("Pressure perturbation $p'(x_{ref, r)$ at $t=0$")
     plt.ylim(0, 25)
     plt.tight_layout()
     plt.plot(x_ref*100, radial_deformation()*1000, 'k-', label='Jet radius $R(x)$')
+    plt.grid()
     plt.legend()
     plt.show()
